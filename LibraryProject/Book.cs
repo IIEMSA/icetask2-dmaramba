662fc709b402780b01857fd7fa8d77ccef48f2df
dmaramba <dumisani.maramba@gmail.com>
2026-03-04 10:40:32 +0200

    Project setup

---
+++ LibraryProject/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject
+{
+    internal class Book
+    {
+
+        /*
+         properties of the book class
+        state they hold information about the book 
+         */
+        public string Author;
+        public string Title;
+        public int Year;
+        public string Description;
+        public string Genre;
+
+        /*
+         Method to display the book information
+         */
+        public void DisplayBookInfo()
+        {
+            Console.WriteLine($"Author: {Author}");
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Year: {Year}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Genre: {Genre}");
+        }
+    }
+}
---
+++ LibraryProject/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using LibraryProject;
+
+Book book = new Book();
+book.Author = "J.K. Rowling";
+book.Title = "Harry Potter and the Philosopher's Stone";
+book.Year = 1997;
+book.Description = "A young wizard's journey begins.";
+book.Genre = "Fantasy";
+
+Book book2 = new Book();
+book2.Author = "George Orwell";
+book2.Title = "1984";
+book2.Year = 1949;
+book2.Description = "A dystopian novel about totalitarianism.";
+book2.Genre = "Science Fiction";
+
+Console.WriteLine("Book 1 Details");
+Console.WriteLine($"Author: {book.Author}");
+Console.WriteLine($"Title: {book.Title}");
+
+//formating the output to make it more readable
+Console.WriteLine(new string('-', 20));
+Console.WriteLine("Book 2 Details");
+Console.WriteLine($"Author: {book2.Author}");
+Console.WriteLine($"Title: {book2.Title}");
+Console.WriteLine(new string('-', 20));
+
+
+//Display books using the method in Book class
+Console.WriteLine("Displaying Book 1 Information:");
+book.DisplayBookInfo();
+Console.WriteLine("\n");
+Console.WriteLine("Displaying Book 2 Information:");
+book2.DisplayBookInfo();
+
+Console.ReadKey();
